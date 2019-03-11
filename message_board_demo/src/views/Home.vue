36c5f4c45a9d5602f7937c29f03d0051024f65f3
LinStan <596636876@qq.com>
2019-03-11 21:21:23 +0800

    留言板demo

---
+++ message_board_demo/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <router-view />
+    <ul>
+      <!-- name是route中的children里的name -->
+      <li><router-link to="/home/list">新闻列表</router-link></li>
+      <!-- 有点问题，无法通过name调用 -->
+      <!-- <li><router-link to="{name:'User'}">个人中心</router-link></li> -->
+      <li><router-link to="/home/user">个人中心</router-link></li>
+      <li><router-link to="/add">添加</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home'
+}
+</script>
---
+++ message_board_demo/src/views/List.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <ul>
+      <!-- item和index不能写反。第一个是内容，第二个是索引。写反了会将lists的内容作为索引 -->
+      <li v-for="(item, index) in pagelists" :key="index">
+        {{ item.title }}-{{ item.content }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import store from '@/store'
+export default {
+  name: 'List',
+  store,
+  computed: {
+    pagelists () {
+      // 数据有取过来。只是展示的问题了
+      console.log(store.state.lists)
+      return store.state.lists
+    }
+  }
+}
+</script>
+<style scoped>
+</style>

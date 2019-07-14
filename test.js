eac1f5c6e7b2d63b73b945d0b5c5792fbb161b02
LinStan <596636876@qq.com>
2019-07-14 09:46:37 +0800

    vue生命周期

--- test.js
+++ test.js
@@ -1,9 +1,22 @@
-function testable(target) {
-  target.isTestable = true;
-  //也可以修改类的原型，
-  target.prototype.isTestable = true
+var zdggzc = function (s1, s2)​ {
+  let l1 = s1.length,
+    l2 = s2.length,
+    ret = '';
+  for (let i = 0; i < l1; i++)​ {
+    for (let j = 0; j < l2; j++)​ {
+      if (s1[i] == s2[j])​ {
+        for (let k = i; k < l1; k++)​ {
+          if (s1[k] != s2[k])​​ {
+            if (k - i - 1 > ret.length)​ {
+              ret = s1.slice(i, k)​​
+              console.log(ret)
+            }​
+            break;​
+          }​
+        }​
+      }​
+    }​
+  }
+  return ret​​
 }
-@testable
-class MyTestableClass {}
-// 上面的这一句等同于 MyTestableClass = testable(MyTestableClass)
-console.log(MyTestableClass.isTestable) // true
+console.log(zdggzc('abcsdg', 'gwegewsds'))
